d7afdcca12cff79541f3da0cc109a2b94421fd3a
jetforce <jetvirtusio@hotmail.com>
2016-03-31 22:09:45 +0800

    First commit

---
+++ src/sheepclient/SheepClient.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sheepclient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jet
+ */
+public class SheepClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        
+ 
+        try {
+            Socket soc = new Socket("localhost",1108);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            String line;
+            String[] list;
+            
+            try {
+                while((line= reader.readLine()) != null){
+
+                    list = line.split(",");
+                    for(String i: list){
+                        System.out.print(i+" ,");
+                    }
+                    System.out.println();
+                    //updateseep
+
+                }            
+            } catch (IOException ex) {
+                Logger.getLogger(SheepClient.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        
+        
+        
+    }   catch (IOException ex) {
+            Logger.getLogger(SheepClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }    
+    
+}
