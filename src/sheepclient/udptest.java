978d62df3a89fe3f0f1f4a1789cf87059ea21c74
ebackers <arcestalavera@gmail.com>
2016-04-15 03:43:48 +0800

    dummies connect

--- src/sheepclient/udptest.java
+++ src/sheepclient/udptest.java
@@ -12,11 +12,13 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.Server;
 import model.Sheep;
 import protocol.UDPProtocol;
 
@@ -29,18 +31,24 @@ public class udptest extends Thread {
     private UDPProtocol protocol = null;
     private DatagramSocket socket;
     private DatagramPacket packet;
+    private static DatagramSocket connectSocket;
+    private ArrayList<Server> serverList;
     private final String address = "localhost";
     private final int port = 1108;
     private GUI ui;
     private int my_id;
+    private Server server;
 
     public udptest(DatagramSocket soc) throws IOException {
         socket = soc;
         byte[] buffer = new byte[1024 * 1];
         ByteBuffer bf = ByteBuffer.wrap(buffer);
         bf.putLong(-1);
-        InetAddress receiverAddress = InetAddress.getByName(address);
-        this.packet = new DatagramPacket(buffer, buffer.length, receiverAddress, port);
+        
+        serverList = getServers();
+        server = tryConnectServer();
+        InetAddress receiverAddress = server.getAddress();
+        this.packet = new DatagramPacket(buffer, buffer.length, receiverAddress, server.getPort());
     }
 
     public void tryConnect() {
@@ -144,4 +152,52 @@ public class udptest extends Thread {
         this.socket.send(this.packet);
     }
 
+    public ArrayList<Server> getServers() {
+        ArrayList<Server> servers = new ArrayList<>();
+
+        Server s = new Server(address, 1108);
+        servers.add(s);
+        s = new Server(address, 1109);
+        servers.add(s);
+        s = new Server(address, 1236);
+        servers.add(s);
+
+        return servers;
+    }
+
+    public Server tryConnectServer() {
+        Server connectServer = null;
+        DatagramPacket connectPacket;
+        byte[] send = new byte[10];
+        byte[] receive = new byte[10];
+        ByteBuffer bb = ByteBuffer.wrap(send);
+        bb.putLong(-2);
+
+        try {
+            connectSocket = new DatagramSocket();
+
+            for (Server s : serverList) {
+                connectPacket = new DatagramPacket(send, send.length, s.getAddress(), s.getPort());
+                connectSocket.send(connectPacket);
+            }
+
+            connectPacket = new DatagramPacket(receive, receive.length);
+
+            connectSocket.receive(connectPacket);
+            bb = ByteBuffer.wrap(connectPacket.getData());
+
+            int id = bb.getInt();
+            connectServer = new Server(connectPacket.getAddress(), connectPacket.getPort());
+            connectServer.setId(id);
+            System.out.println(id);
+
+        } catch (SocketException ex) {
+            Logger.getLogger(UDPProtocol.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(UDPProtocol.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(UDPProtocol.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return connectServer;
+    }
 }
